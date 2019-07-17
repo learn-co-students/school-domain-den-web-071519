f97c9abf4c0371a1e372573382495de464f46a58
SwanHub <jdprince555@gmail.com>
2019-07-16 19:08:12 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :roster
+
+   def initialize(school)
+     @school = school
+     @roster = {}
+   end
+
+   def add_student(student_name, grade)
+     if @roster.keys.include?(grade)
+       @roster[grade].push(student_name)
+     else
+     @roster[grade] = [student_name]
+    end
+   end
+
+   def grade(grade)
+     @roster[grade]
+   end
+
+   def sort
+    @roster = @roster.each do |grade, students|
+      students.sort!
+      # binding.pry
+    end
+   end
+
+end
